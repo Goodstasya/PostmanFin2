d0b7cbe90e12b278235afbb15dcbf3ed4395b7df
Ilina Anastasiia <goodstasya@gmail.com>
2020-12-18 23:30:14 +0400

    CorrectTest

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -25,7 +25,7 @@ public class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"))
+                .body("data", equalTo("some data"))
         ;
     }
 
